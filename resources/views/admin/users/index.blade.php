f6bd3e4f773d98ecc0f5dff9e3e2bbddb94f984a
Niloy Quazi <niloy940@gmail.com>
2020-08-31 07:16:51 +0600

    assign rule to users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -9,9 +9,38 @@
                 <div class="card-header">Users</div>
 
                 <div class="card-body">
-                   @foreach($users as $user)
-                        {{ $user->name }} - {{ $user->email }}
-                   @endforeach
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Roles</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        
+                        <tbody>
+                        @foreach($users as $user)
+                            <tr>
+                                <th scope="row">{{ $user->id }}</th>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ implode(', ', $user->roles()->get()->pluck('name')->toArray()) }}</td>
+                                <td>
+                                    <a href="{{ route('admin.users.edit', $user->id) }}"><button type="button" class="btn btn-primary float-left">Edit</button></a>
+                                    
+                                    <form method="POST" action="{{ route('admin.users.destroy', $user) }}" class="float-left">
+                                        @method('delete')
+                                        @csrf
+                                            
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach    
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
