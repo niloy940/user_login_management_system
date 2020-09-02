d1f7405c2d05a7d2e9230567754342699bfe8ac3
Niloy Quazi <niloy940@gmail.com>
2020-09-02 06:59:31 +0600

    redirect users based on rules

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,7 +26,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Gate::define('manage-users', function ($user) {
-            return $user->hasAnyRoles(['admin', 'author']);
+            return $user->hasAnyRoles(['admin', 'author', 'users']);
         });
 
         Gate::define('edit-users', function ($user) {
